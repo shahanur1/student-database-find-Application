68adbe7c69278e75ab2f839d01ed0ee71fb4c980
Shahanur Islam Shagor <65355847+Shahanur1231@users.noreply.github.com>
2020-06-13 21:44:39 +0600

    first commit

---
+++ student database find.py
@@ -0,0 +1,15 @@
+Information={355501:'shagor',355502:'shahanur',355503:'islam',355504:'akash',355505:'jaki',355506:'jamiul',355507:'jakir',355508:'shahjalal',355509:'shaju',355510:'raju',
+               355511:'jibon',355512:'ahammed',355513:'rahaman',355514:'shil',355515:'jakia',355516:'jala',355517:'sulta',355518:'shapor',355519:'rakib',355520:'ronju',
+               355521:'ror',355522:'shnur',355523:'lam',355524:'sh',355525:'jki',355526:'jaul',355527:'jir',355528:'hjalal',355529:'haju',355530:'rju',
+               355531:'shagor',355532:'shahanur',355533:'islam',355534:'akash',355535:'jaki',355536:'jamiul',355537:'kir',355538:'shjalal',355539:'su',355540:'ru',
+               355541:'shagor',355542:'shahanur',355543:'islam',355544:'akash',355545:'jaki',355546:'jamiul',355547:'jakir',355548:'shahjalal',355549:'shaju',355550:'raju',
+               355551:'shagor',355552:'shahanur',355553:'islam',355554:'akash',355555:'jaki',355556:'jamiul',355557:'jakir',355558:'shahjalal',355559:'shaju',355560:'raju',
+               355561:'shagor',355562:'shahanur',355563:'islam',355564:'akash',355565:'jaki',355566:'jamiul',355567:'jakir',355568:'shahjalal',355569:'shaju',355570:'raju',
+               355571:'shagor',355572:'shr',355573:'islam',355574:'akash',355575:'jaki',355576:'jamiul',355577:'jakir',355578:'shahjalal',355579:'shaju',355578:'raju',
+               355581:'shagor',355582:'shahanur',355583:'islam',355584:'akash',355585:'jaki',355586:'jamiul',355587:'jakir',355588:'shahjalal',355589:'shaju',355590:'raju',
+               355591:'j',355592:'s',355593:'islam',355594:'akash',355595:'jaki',355596:'jamiul',355597:'jakir',355598:'shahjalal',355599:'shaju',355600:'raju'}
+roll=int(input("Enter your roll: "))
+if roll in Information:
+    print("Roll: "+str(roll)+(" Name: ")+Information[roll])
+else:
+    print("Roll is not found")
